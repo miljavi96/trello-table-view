e933d462b14dbf90135bc854cf1c52bb6204631a
miljavi96 <programador.siscotec@gmail.com>
2026-09-22 17:50:57 -0300

    fix: use absolute icon urls and follow trello color theme

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
+var BASE_URL = 'https://miljavi96.github.io/trello-table-view';
+
 window.TrelloPowerUp.initialize({
   'board-buttons': function () {
     return [
       {
         icon: {
-          dark: './icon-white.svg',
-          light: './icon-black.svg'
+          dark: BASE_URL + '/icon-white.svg',
+          light: BASE_URL + '/icon-black.svg'
         },
         text: 'Table',
         condition: 'always',
